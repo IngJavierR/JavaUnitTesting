bf74bca30802c8641854a01293b76bfc5abcf6c6
IngJavierR <hazelapd@gmail.com>
2018-04-25 22:48:02 -0500

    Demo events

---
+++ src/main/java/com/howtoprogram/junit5/product/Product.java
@@ -0,0 +1,60 @@
+package com.howtoprogram.junit5.product;
+
+public class Product {
+    private long id;
+    private String name;
+    private double price;
+
+    public Product(long id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Product product = (Product) o;
+
+        if (id != product.id) return false;
+        if (Double.compare(product.price, price) != 0) return false;
+        return name != null ? name.equals(product.name) : product.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = (int) (id ^ (id >>> 32));
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        temp = Double.doubleToLongBits(price);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+}
--- src/test/java/com/howtoprogram/junit5/BasicDemoTest.java
+++ src/test/java/com/howtoprogram/junit5/BasicDemoTest.java
@@ -1,11 +1,31 @@
 package com.howtoprogram.junit5;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BasicDemoTest {
   private BasicDemo basicDemo;
+
+  @BeforeAll
+  static void setUpAll(){
+    System.out.println("BeforeAll");
+  }
+
+  @BeforeEach
+  void setUp(){
+    System.out.println("BeforeEach");
+  }
+
+  @AfterAll
+  static void tearDownAll(){
+    System.out.println("AfterAll");
+  }
+
+  @AfterEach
+  void tearDown(){
+    System.out.println("AfterEach");
+  }
 
   @Test
   void testBasicSalaryWithValidSalary() {
